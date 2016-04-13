d72ff086f228b9780438dad36ce702fb544192da
Mr. Theodoropoulos <tony.theodoropoulos@ed.amdsb.ca>
2016-04-13 16:14:29 -0400

    Initial Commit

---
+++ XMLWeather/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Xml;
+
+namespace XMLWeather
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            // get information about current and forecast weather from the internet
+            GetData();
+
+            // take info from the current weather file and display it to the screen
+            ExtractCurrent();
+
+            // take info from the forecast weather file and display it to the screen
+            ExtractForecast();
+        }
+
+        private static void GetData()
+        {
+            WebClient client = new WebClient();
+
+            string currentFile = "http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
+            string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
+            
+            client.DownloadFile(currentFile, "WeatherData.xml");
+            client.DownloadFile(forecastFile, "WeatherData7Day.xml");
+        }
+
+        private void ExtractCurrent()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load("WeatherData.xml");
+
+            //create a node variable to represent the parent element
+            XmlNode parent;
+            parent = doc.DocumentElement;
+
+            //check each child of the parent element
+            foreach (XmlNode child in parent.ChildNodes)
+            {
+                // TODO if the "city" element is found display the value of it's "name" attribute
+            }
+        }
+              
+        private void ExtractForecast()
+        {
+                     
+        }
+
+
+        private void MakePictureParent()
+        {
+            
+        }
+
+    }
+}
