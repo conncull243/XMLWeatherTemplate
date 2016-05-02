44561e4f84289be0343fa294feedc15a721f8390
conncull243 <conncull243@ed.amdsb.ca>
2016-05-02 09:11:22 -0400

    commit 2

--- XMLWeather/Form1.Designer.cs
+++ XMLWeather/Form1.Designer.cs
@@ -30,27 +30,96 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.cityOutput = new System.Windows.Forms.Label();
+            this.tempOutput = new System.Windows.Forms.Label();
+            this.windDescription = new System.Windows.Forms.Label();
+            this.maxOutput = new System.Windows.Forms.Label();
+            this.minOutput = new System.Windows.Forms.Label();
+            this.cloudOutput = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // cityOutput
             // 
             this.cityOutput.AutoSize = true;
-            this.cityOutput.Location = new System.Drawing.Point(61, 120);
+            this.cityOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cityOutput.Location = new System.Drawing.Point(96, 9);
             this.cityOutput.Name = "cityOutput";
-            this.cityOutput.Size = new System.Drawing.Size(35, 13);
+            this.cityOutput.Size = new System.Drawing.Size(86, 31);
             this.cityOutput.TabIndex = 0;
             this.cityOutput.Text = "label1";
+            // 
+            // tempOutput
+            // 
+            this.tempOutput.AutoSize = true;
+            this.tempOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tempOutput.Location = new System.Drawing.Point(10, 50);
+            this.tempOutput.Name = "tempOutput";
+            this.tempOutput.Size = new System.Drawing.Size(109, 39);
+            this.tempOutput.TabIndex = 1;
+            this.tempOutput.Text = "label1";
+            // 
+            // windDescription
+            // 
+            this.windDescription.AutoSize = true;
+            this.windDescription.Location = new System.Drawing.Point(12, 164);
+            this.windDescription.Name = "windDescription";
+            this.windDescription.Size = new System.Drawing.Size(35, 13);
+            this.windDescription.TabIndex = 2;
+            this.windDescription.Text = "label1";
+            // 
+            // maxOutput
+            // 
+            this.maxOutput.AutoSize = true;
+            this.maxOutput.Location = new System.Drawing.Point(248, 60);
+            this.maxOutput.Name = "maxOutput";
+            this.maxOutput.Size = new System.Drawing.Size(35, 13);
+            this.maxOutput.TabIndex = 3;
+            this.maxOutput.Text = "label1";
+            // 
+            // minOutput
+            // 
+            this.minOutput.AutoSize = true;
+            this.minOutput.Location = new System.Drawing.Point(248, 87);
+            this.minOutput.Name = "minOutput";
+            this.minOutput.Size = new System.Drawing.Size(35, 13);
+            this.minOutput.TabIndex = 4;
+            this.minOutput.Text = "label1";
+            // 
+            // cloudOutput
+            // 
+            this.cloudOutput.AutoSize = true;
+            this.cloudOutput.Location = new System.Drawing.Point(99, 201);
+            this.cloudOutput.Name = "cloudOutput";
+            this.cloudOutput.Size = new System.Drawing.Size(35, 13);
+            this.cloudOutput.TabIndex = 7;
+            this.cloudOutput.Text = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(89, 50);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(149, 139);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(244, 269);
+            this.ClientSize = new System.Drawing.Size(307, 269);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.cloudOutput);
+            this.Controls.Add(this.minOutput);
+            this.Controls.Add(this.maxOutput);
+            this.Controls.Add(this.windDescription);
+            this.Controls.Add(this.tempOutput);
             this.Controls.Add(this.cityOutput);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Central Weather";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -59,7 +128,12 @@
         #endregion
 
         private System.Windows.Forms.Label cityOutput;
-
+        private System.Windows.Forms.Label tempOutput;
+        private System.Windows.Forms.Label windDescription;
+        private System.Windows.Forms.Label maxOutput;
+        private System.Windows.Forms.Label minOutput;
+        private System.Windows.Forms.Label cloudOutput;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- XMLWeather/Form1.cs
+++ XMLWeather/Form1.cs
@@ -33,7 +33,7 @@ namespace XMLWeather
 
             string currentFile = "http://api.openweathermap.org/data/2.5/weather?q=Stratford,CA&mode=xml&units=metric&appid=3f2e224b815c0ed45524322e145149f0";
             string forecastFile = "http://api.openweathermap.org/data/2.5/forecast/daily?q=Stratford,CA&mode=xml&units=metric&cnt=7&appid=3f2e224b815c0ed45524322e145149f0";
-            
+
             client.DownloadFile(currentFile, "WeatherData.xml");
             client.DownloadFile(forecastFile, "WeatherData7Day.xml");
         }
@@ -51,19 +51,103 @@ namespace XMLWeather
             foreach (XmlNode child in parent.ChildNodes)
             {
                 // TODO if the "city" element is found display the value of it's "name" attribute
+                if (child.Name == "city")
+                {
+                    cityOutput.Text = child.Attributes["name"].Value;
+                }
+                if (child.Name == "temperature")
+                {
+                    tempOutput.Text = child.Attributes["value"].Value;
+                }
+                if (child.Name == "wind")
+                {
+                    foreach (XmlNode grandchild in child.ChildNodes)
+                    {
+                        if (grandchild.Name == "speed")
+                        {
+                            windDescription.Text = grandchild.Attributes["name"].Value;
+                        }
+                    }
+                }
             }
         }
-              
+
         private void ExtractForecast()
         {
-                     
+            XmlDocument doc = new XmlDocument();
+            doc.Load("WeatherData7Day.xml");
+
+            XmlNode parent;
+            parent = doc.DocumentElement;
+            int day = 1;
+
+            foreach (XmlNode child in parent.ChildNodes)
+            {
+                if (child.Name == "forecast")
+                {
+                    foreach (XmlNode grandchild in child.ChildNodes)
+                    {
+                        foreach (XmlNode greatGrandchild in grandchild.ChildNodes)
+                        {
+                            if (greatGrandchild.Name == "symbol")
+                            {
+                                int number = Convert.ToInt32(greatGrandchild.Attributes["number"].Value);
+
+                                if (number >= 500 && number < 800)
+                                {
+                                    pictureBox1.Image = Properties.Resources.Rain3;
+                                }
+                                if (number >= 800)
+                                {
+                                    pictureBox1.Image = Properties.Resources.cartoon_sun;
+                                }
+                                if (number < 500)
+                                {
+                                    pictureBox1.Image = Properties.Resources.cartoon_cloud;
+                                }
+                            }
+                            if (greatGrandchild.Name == "temperature")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        maxOutput.Text = greatGrandchild.Attributes["max"].Value;
+                                        minOutput.Text = greatGrandchild.Attributes["min"].Value;
+                                        break;
+                                    //case 2:
+                                    //    max2Output.Text = greatGrandchild.Attributes["max"].Value;
+                                    //    min2Output.Text = greatGrandchild.Attributes["min"].Value;
+                                    //    break;
+                                    default:
+                                        break;
+                                }
+                            }
+                            if (greatGrandchild.Name == "clouds")
+                            {
+                                switch (day)
+                                {
+                                    case 1:
+                                        cloudOutput.Text = greatGrandchild.Attributes["value"].Value;
+                                        day++;
+                                        break;
+                                    //case 2:
+                                    //    clouds2Output.Text = greatGrandchild.Attributes["value"].Value;
+                                    //    day++;
+                                    //    break;
+                                    default:
+                                        break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
-
-
         private void MakePictureParent()
         {
-            
-        }
 
+
+        }
     }
 }
+
